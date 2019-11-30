9d9ba9837b9ad1fea86f9c71685242464f283c6f
Adrien Turchini <adrien.turchini@me.com>
2019-11-30 12:24:18 +0100

    test

--- Monopoly_DesignPatternA4/Program.cs
+++ Monopoly_DesignPatternA4/Program.cs
@@ -6,7 +6,7 @@ namespace Monopoly_DesignPatternA4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
         }
     }
 }
